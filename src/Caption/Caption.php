00848c95e4666e01400ea533adbc38df23731be6
Chauncey McAskill <shimoroka@gmail.com>
2015-04-27 02:08:34 -0400

    Caption now uses MutableAttributesTrait.

--- src/Caption/Caption.php
+++ src/Caption/Caption.php
@@ -4,15 +4,19 @@ namespace Donquixote\Cellbrush\Caption;
 
 use Donquixote\Cellbrush\Html\Element;
 use Donquixote\Cellbrush\Html\ContentTrait;
+use Donquixote\Cellbrush\Html\MutableAttributesInterface;
+use Donquixote\Cellbrush\Html\MutableAttributesTrait;
 
 class Caption extends Element {
 
-  use ContentTrait;
+  use ContentTrait, MutableAttributesTrait;
 
   /**
    * @param string $content
    */
   function __construct($content = '') {
+    $this->__constructMutableAttributes();
+
     parent::__construct('caption', $content);
   }
 
